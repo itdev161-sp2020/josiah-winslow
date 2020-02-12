ece8db81520e550ee49033ee4d18e225cccf3ae5
WinslowJosiah <winslowjosiah@gmail.com>
2020-02-12 12:30:12 -0600

    Added database connection

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 import express from "express";
+import connectDatabase from "./config/db";
 
 // Init express application
 const app = express();
+
+// Connect database
+connectDatabase();
 
 // API endpoints
 app.get("/", (req, res) => {
