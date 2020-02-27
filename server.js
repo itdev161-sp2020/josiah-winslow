e3ca902d687b0c7111992b7ed84d9555e2fd3ef3
WinslowJosiah <winslowjosiah@gmail.com>
2020-02-27 17:04:32 -0600

    Created POST endpoint

--- server.js
+++ server.js
@@ -7,11 +7,18 @@ const app = express();
 // Connect database
 connectDatabase();
 
+// Configure Middleware
+app.use(express.json({ extended: false }));
+
 // API endpoints
 app.get("/", (req, res) => {
     res.send("http get request to root api endpoint");
 });
 
-// Connection listener
+app.post("/api/users", (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
 
+// Connection listener
 app.listen(3000, () => console.log("Express server running on port 3000"));
