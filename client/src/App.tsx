f9edb2b9eb2710bcb9a015c7e023c49c38ceb946
WinslowJosiah <winslowjosiah@gmail.com>
2020-05-16 00:19:35 -0500

    Updated to fetch post data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,22 +8,12 @@ import Login from "./components/Login/Login";
 
 class App extends React.Component {
   state = {
-    data: null,
+    posts: [],
     token: null,
     user: null
   }
 
   componentDidMount() {
-    axios.get("http://localhost:5000")
-    .then((response) => {
-      this.setState({
-        data: response.data
-      })
-    })
-    .catch((error) => {
-      console.error(`Error fetching data: ${error}`);
-    })
-
     this.authenticateUser();
   }
 
@@ -44,13 +34,42 @@ class App extends React.Component {
       axios.get("http://localhost:5000/api/auth", config)
         .then((response) => {
           localStorage.setItem("user", response.data.name);
-          this.setState({ user: response.data.name });
+          this.setState(
+            {
+              user: response.data.name,
+              token: token
+            },
+            () => {
+              this.loadData();
+            }
+          );
         })
         .catch((error) => {
           localStorage.removeItem("user");
           this.setState({ user: null });
           console.error(`Error logging in: ${error}`);
         })
+    }
+  }
+
+  loadData = () => {
+    const { token } = this.state;
+
+    if (token) {
+      const config = {
+        headers: {
+          "x-auth-token": token
+        }
+      };
+      axios.get("http://localhost:5000/api/posts", config)
+      .then(response => {
+        this.setState({
+          posts: response.data
+        });
+      })
+      .catch(error => {
+        console.error(`Error fetching data: ${error}`);
+      });
     }
   }
 
@@ -61,7 +80,7 @@ class App extends React.Component {
   }
 
   render() {
-    let { user, data } = this.state;
+    let { user, posts } = this.state;
     const authProps = {
       authenticateUser: this.authenticateUser
     }
@@ -88,15 +107,23 @@ class App extends React.Component {
           </header>
           <main>
             <Route exact path="/">
-              {user ?
+              {user ? (
                 <React.Fragment>
                   <div>Hello {user}!</div>
-                  <div>{data}</div>
-                </React.Fragment> :
+                  <div>
+                    {posts.map(post => {
+                      <div key={post._id}>
+                        <h1>{post.title}</h1>
+                        <p>{post.body}</p>
+                      </div>
+                    })}
+                  </div>
+                </React.Fragment>
+              ) : (
                 <React.Fragment>
                   Please Register or Login
                 </React.Fragment>
-              }
+              )}
             </Route>
             <Switch>
               <Route
